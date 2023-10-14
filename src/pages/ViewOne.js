9a89f83bf58c294d65e7843f4ab6ede56361f420
Anthony Bosek <anthony.bosek@gmail.com>
2023-10-14 15:42:41 -0700

    sidenav / topnav

--- src/pages/ViewOne.js
+++ src/pages/ViewOne.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 const ViewOne = () => {
   const { id } = useParams();
+  const [dog, setDog] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3005/dogs/${id}`)
+      .then((resp) => resp.json())
+      .then(setDog);
+  }, [id]);
+
   return (
-    <div>
-      <h1>View One</h1>
+    <div className="dogPage">
+      <h1>{dog.name}</h1>
+      <img src={dog.image} alt={dog.name} />
+      <p>Breed: {dog.breed}</p>
+      <p>Age: {dog.age}</p>
+      <p>Gender: {dog.gender}</p>
+      <p>
+        About: <br /> {dog.bio}
+      </p>
+      <p>Owner: {dog.owner}</p>
       <p>Viewing dog with id: {id}</p>
       <Link to={`/edit/${id}`}>
         <button>Edit</button>
