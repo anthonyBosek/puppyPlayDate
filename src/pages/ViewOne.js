66c636b56d9095b9d4f01f3c2f657e1151850cea
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-14 16:08:38 -0700

    Add basic styling and info to ViewOne

--- src/pages/ViewOne.js
+++ src/pages/ViewOne.js
@@ -1,9 +1,26 @@
+import { useEffect,useState } from "react";
 import { Link, useParams } from "react-router-dom";
 const ViewOne = () => {
+  //use state of "" so no loading errors for name not existing
+  const [dog, setDog] = useState({name:""}); 
+
+  useEffect(()=>{
+    fetch(`http://localhost:3005/dogs/${id}`)
+    .then(resp => resp.json())
+    .then(setDog)
+  },[])
+  
+
   const { id } = useParams();
   return (
-    <div>
-      <h1>View One</h1>
+    <div className="dogPage">
+      <h1>{`${dog.name}${dog.name.endsWith('s') ? ' page' : "'s page"}`}</h1>
+      <img src={dog.image} alt={dog.name}/>
+      <p>Breed: {dog.breed}</p>
+      <p>Age: {dog.age}</p>
+      <p>Gender: {dog.gender}</p>
+      <p>About: <br/> {dog.bio}</p>
+      <p>Owner: {dog.owner}</p>
       <p>Viewing dog with id: {id}</p>
       <Link to={`/edit/${id}`}>
         <button>Edit</button>
