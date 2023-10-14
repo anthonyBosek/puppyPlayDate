a8d4c37f08d875d06579c17958ed318da53e69f2
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-14 15:42:24 -0700

    Add real data to dog cards and center

--- src/pages/ViewAll.js
+++ src/pages/ViewAll.js
@@ -1,25 +1,34 @@
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
 // temporary to see things work...
-const dogs = [
-  {
-    id: 1,
-    name: "Fido",
-    age: 3,
-  },
-  {
-    id: 2,
-    name: "Rover",
-    age: 4,
-  },
-  {
-    id: 3,
-    name: "Rex",
-    age: 6,
-  },
-];
+// const dogs = [
+//   {
+//     id: 1,
+//     name: "Fido",
+//     age: 3,
+//   },
+//   {
+//     id: 2,
+//     name: "Rover",
+//     age: 4,
+//   },
+//   {
+//     id: 3,
+//     name: "Rex",
+//     age: 6,
+//   },
+// ];
 // end temporary
 
 const ViewAll = () => {
+  const [dogs,setDogs] = useState([])
+  
+  useEffect(()=>{
+    fetch("http://localhost:3005/dogs")
+    .then(resp => resp.json())
+    .then(setDogs)
+  },[])
+
   const allDogs = dogs.map((dog) => <Card key={dog.id} dog={dog} />);
   return (
     <div>
