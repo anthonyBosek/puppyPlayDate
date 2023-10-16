6db6fb711288f08528deee94af4e33f8eed9a08c
Kat Tannehill <katetannehill@gmail.com>
2023-10-16 11:38:25 -0600

    View One styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,7 @@ const Card = ({ dog }) => {
   return (
     <div className="dog-card">
       <img className="dog-img" src={dog.image} alt={dog.name} />
-      <h2>{dog.name}</h2>
-      <p>{dog.age}</p>
+      <h2>{dog.name}, <span>{dog.age}</span></h2>
       <Link to={`/dogs/${dog.id}`}>
         <button className="btn-small bg-blue">View</button>
       </Link>
