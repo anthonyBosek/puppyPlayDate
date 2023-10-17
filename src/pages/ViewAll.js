775e0aba62986f9e9d814c41e3b1b6c589818bb3
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-17 14:07:57 -0700

    Fix filter functionality

--- src/pages/ViewAll.js
+++ src/pages/ViewAll.js
@@ -6,8 +6,8 @@ import { useEffect, useState } from "react";
 const ViewAll = () => {
   const { searchTerm } = useOutletContext();
   const [dogs, setDogs] = useState([]);
-  // const unmatches = JSON.parse(localStorage.unmatches || "[]");
-  // const blockedIds = unmatches.map((doggy) => doggy.id);
+  const unmatches = JSON.parse(localStorage.unmatches || "[]");
+  const blockedIds = unmatches.map((doggy) => doggy.id);
 
   useEffect(() => {
     fetch("http://localhost:3005/dogs")
@@ -16,7 +16,7 @@ const ViewAll = () => {
   }, []);
 
   const allDogs = dogs
-    .filter((dog) => dog.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    .filter((dog) => dog.name.toLowerCase().includes(searchTerm.toLowerCase()) && !(blockedIds.includes(dog.id)))
     .map((dog) => <Card key={dog.id} dog={dog} />);
 
   return (
