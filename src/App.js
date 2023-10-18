e58b79d1e76969dda16e1dbad4cbf4d82686524a
Kat Tannehill <katetannehill@gmail.com>
2023-10-17 18:11:50 -0600

    Add snack bar to form and fetch call

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import AlertBar from "./components/AlertBar";
 
 const App = () => {
   const [isDark, setIsDark] = useState(localStorage.dark === "true");
   const [searchTerm, setSearchTerm] = useState("");
   const [authID, setAuthID] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [snackType, setSnackType] = useState("");
 
   const toggleDarkMode = () => {
     setIsDark(!isDark);
@@ -19,13 +22,28 @@ const App = () => {
 
   const authUserID = (id) => setAuthID(id);
 
-  const ctx = { authUserID, searchTerm };
+  const setAlertMessage = (msg) => {
+    setMessage(msg)
+  }
+
+  const handleSnackType = (type) => {
+    setSnackType(type)
+  }
+
+  const ctx = { authUserID, searchTerm, setAlertMessage, handleSnackType };
   // const ctx = [authUserID, searchTerm];
 
   return (
     <div className={isDark ? "app dark" : "app"}>
       <SideNav authID={authID} />
       <main className="container">
+        {message && 
+          <AlertBar 
+            message={message} 
+            setAlertMessage={setAlertMessage} 
+            snackType={snackType} 
+            handleSnackType={handleSnackType}
+          />}
         <TopNav
           isDark={isDark}
           toggleDarkMode={toggleDarkMode}
